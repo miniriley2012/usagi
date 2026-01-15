4f5d0d3619942c6a2db51b6088984547206180b2
Riley Quinn <merileyquinn@gmail.com>
2026-01-15 11:07:51 -0600

    add ast package

---
+++ internal/compile/ast/ast.go
@@ -0,0 +1,186 @@
+package ast
+
+import "codeberg.org/rileyq/usagi/internal/compile/token"
+
+type Node interface {
+	Pos() token.Pos
+	End() token.Pos
+
+	astNode()
+}
+
+type Expr interface {
+	Node
+
+	astExpr()
+}
+
+type Stmt interface {
+	Node
+
+	astStmt()
+}
+
+type Module struct {
+	Name     string
+	Bindings []*Binding
+}
+
+func (m *Module) Pos() token.Pos { return token.NoPos }
+func (m *Module) End() token.Pos { return token.NoPos }
+
+func (m *Module) astNode() {}
+
+type BindingMode int
+
+const (
+	ModeExport BindingMode = 1 << iota
+	ModeConst
+	ModeFunc
+)
+
+func (m BindingMode) Export() bool { return m&ModeExport != 0 }
+func (m BindingMode) Const() bool  { return m&ModeConst != 0 }
+func (m BindingMode) Func() bool   { return m&ModeFunc != 0 }
+
+type Binding struct {
+	Mode  BindingMode
+	Name  *Identifier
+	Type  Expr
+	Value Expr
+}
+
+type Identifier struct {
+	NamePos token.Pos
+	NameEnd token.Pos
+	Name    string
+}
+
+func (id *Identifier) Pos() token.Pos { return id.NamePos }
+func (id *Identifier) End() token.Pos { return id.NameEnd }
+
+func (id *Identifier) astNode() {}
+func (id *Identifier) astExpr() {}
+
+type CallExpr struct {
+	Base Expr
+	Args []Expr
+}
+
+func (expr *CallExpr) Pos() token.Pos { return expr.Base.Pos() }
+func (expr *CallExpr) End() token.Pos { return expr.Base.End() }
+
+func (expr *CallExpr) astNode() {}
+func (expr *CallExpr) astExpr() {}
+
+type Literal struct {
+	Tok   token.Type
+	Value string
+}
+
+func (expr *Literal) Pos() token.Pos { panic("TODO") }
+func (expr *Literal) End() token.Pos { panic("TODO") }
+
+func (expr *Literal) astNode() {}
+func (expr *Literal) astExpr() {}
+
+type FuncExpr struct {
+	Params     []*Param
+	ReturnType Expr
+	Body       *BlockExpr
+}
+
+func (expr *FuncExpr) Pos() token.Pos { panic("TODO") }
+func (expr *FuncExpr) End() token.Pos { panic("TODO") }
+
+func (expr *FuncExpr) astNode() {}
+func (expr *FuncExpr) astExpr() {}
+
+type Param struct {
+	Name *Identifier
+	Type Expr
+}
+
+func (p *Param) Pos() token.Pos { panic("TODO") }
+func (p *Param) End() token.Pos { panic("TODO") }
+
+func (p *Param) astNode() {}
+
+type BlockExpr struct {
+	List []Stmt
+}
+
+func (expr *BlockExpr) Pos() token.Pos { panic("TODO") }
+func (expr *BlockExpr) End() token.Pos { panic("TODO") }
+
+func (*BlockExpr) astNode() {}
+func (*BlockExpr) astExpr() {}
+
+type ReturnExpr struct {
+	Value Expr
+}
+
+func (expr *ReturnExpr) Pos() token.Pos { panic("TODO") }
+func (expr *ReturnExpr) End() token.Pos { panic("TODO") }
+
+func (*ReturnExpr) astNode() {}
+func (*ReturnExpr) astExpr() {}
+
+type BinaryExpr struct {
+	Left  Expr
+	Op    token.Type
+	Right Expr
+}
+
+func (expr *BinaryExpr) Pos() token.Pos { return expr.Left.Pos() }
+func (expr *BinaryExpr) End() token.Pos { return expr.Right.End() }
+
+func (*BinaryExpr) astNode() {}
+func (*BinaryExpr) astExpr() {}
+
+type ExprStmt struct{ X Expr }
+
+func (stmt *ExprStmt) Pos() token.Pos { return stmt.X.Pos() }
+func (stmt *ExprStmt) End() token.Pos { return stmt.X.End() }
+
+func (*ExprStmt) astNode() {}
+func (*ExprStmt) astStmt() {}
+
+type MemberExpr struct {
+	Base   Expr
+	Member *Identifier
+}
+
+func (expr *MemberExpr) Pos() token.Pos { return expr.Base.Pos() }
+func (expr *MemberExpr) End() token.Pos { return expr.Member.End() }
+
+func (*MemberExpr) astNode() {}
+func (*MemberExpr) astExpr() {}
+
+type SliceExpr struct {
+	Base Expr
+}
+
+func (expr *SliceExpr) Pos() token.Pos { panic("TODO") }
+func (expr *SliceExpr) End() token.Pos { return expr.Base.End() }
+
+func (*SliceExpr) astNode() {}
+func (*SliceExpr) astExpr() {}
+
+type ManyPointerExpr struct {
+	Base Expr
+}
+
+func (expr *ManyPointerExpr) Pos() token.Pos { panic("TODO") }
+func (expr *ManyPointerExpr) End() token.Pos { return expr.Base.End() }
+
+func (*ManyPointerExpr) astNode() {}
+func (*ManyPointerExpr) astExpr() {}
+
+type VarArgExpr struct{}
+
+func (*VarArgExpr) Pos() token.Pos { panic("TODO") }
+func (*VarArgExpr) End() token.Pos { panic("TODO") }
+
+func (*VarArgExpr) astNode() {}
+func (*VarArgExpr) astExpr() {}
