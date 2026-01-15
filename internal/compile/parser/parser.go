0c6de410d4b907741663b613535e234313243e38
Riley Quinn <merileyquinn@gmail.com>
2026-01-15 15:08:47 -0600

    add parser package

---
+++ internal/compile/parser/parser.go
@@ -0,0 +1,388 @@
+package parser
+
+import (
+	"errors"
+	"fmt"
+	"io"
+
+	"codeberg.org/rileyq/usagi/internal/compile/ast"
+	"codeberg.org/rileyq/usagi/internal/compile/scanner"
+	"codeberg.org/rileyq/usagi/internal/compile/token"
+)
+
+type Parser struct {
+	scn Scanner
+	t   *token.Token
+}
+
+func (p *Parser) Parse(name string) *ast.Module {
+	p.next()
+
+	var bindings []*ast.Binding
+
+	for p.t != nil {
+		bindings = append(bindings, p.binding())
+	}
+
+	return &ast.Module{Name: name, Bindings: bindings}
+}
+
+func (p *Parser) binding() *ast.Binding {
+	var mode ast.BindingMode
+	var typ ast.Expr
+	var val ast.Expr
+
+	if p.accept(token.Export) != nil {
+		mode |= ast.ModeExport
+	}
+
+	if p.accept(token.Const) != nil {
+		mode |= ast.ModeConst
+	}
+
+	if mode&ast.ModeConst != 0 && p.t.Type == token.Identifier {
+		name := p.identifier()
+
+		if p.accept(token.Colon) != nil {
+			typ = p.expr()
+		}
+
+		if p.accept(token.Assign) != nil {
+			val = p.expr()
+		}
+
+		p.expect(token.Semicolon)
+
+		return &ast.Binding{
+			Mode:  mode,
+			Name:  name,
+			Type:  typ,
+			Value: val,
+		}
+	}
+
+	switch p.t.Type {
+	case token.Let:
+		return p.letBinding(mode)
+	case token.Func:
+		return p.funcBinding(mode | ast.ModeFunc)
+	default:
+		panic("TODO")
+	}
+}
+
+func (p *Parser) letBinding(mode ast.BindingMode) *ast.Binding {
+	var typ ast.Expr
+	var val ast.Expr
+
+	p.expect(token.Let)
+
+	name := p.identifier()
+
+	if p.accept(token.Colon) != nil {
+		typ = p.expr()
+	}
+
+	if p.accept(token.Assign) != nil {
+		val = p.expr()
+	}
+
+	return &ast.Binding{
+		Mode:  mode,
+		Name:  name,
+		Type:  typ,
+		Value: val,
+	}
+}
+
+func (p *Parser) funcBinding(mode ast.BindingMode) *ast.Binding {
+	p.expect(token.Func)
+
+	name := p.identifier()
+
+	fn := p.funcBody()
+
+	return &ast.Binding{
+		Mode:  mode,
+		Name:  name,
+		Type:  nil,
+		Value: fn,
+	}
+}
+
+func (p *Parser) funcBody() *ast.FuncExpr {
+	var params []*ast.Param
+	var body *ast.BlockExpr
+
+	p.expect(token.OpenParen)
+	for {
+		if p.accept(token.CloseParen) != nil {
+			break
+		}
+		params = append(params, p.param())
+		if p.accept(token.Comma) != nil {
+			continue
+		} else if p.accept(token.CloseParen) != nil {
+			break
+		} else {
+			p.expect(token.CloseParen)
+		}
+	}
+
+	returnType := p.expr()
+
+	if p.t.Type == token.OpenBrace {
+		body = p.blockExpr()
+	}
+
+	return &ast.FuncExpr{
+		Params:     params,
+		ReturnType: returnType,
+		Body:       body,
+	}
+}
+
+func (p *Parser) blockExpr() *ast.BlockExpr {
+	var stmts []ast.Stmt
+
+	p.expect(token.OpenBrace)
+	for {
+		if p.accept(token.CloseBrace) != nil {
+			break
+		}
+		stmts = append(stmts, p.stmt())
+	}
+
+	return &ast.BlockExpr{List: stmts}
+}
+
+func (p *Parser) stmt() ast.Stmt {
+	switch p.t.Type {
+	case token.Return, token.Identifier:
+		x := p.expr()
+		p.expect(token.Semicolon)
+		return &ast.ExprStmt{X: x}
+	case token.If:
+		x := p.expr()
+		return &ast.ExprStmt{X: x}
+	default:
+		panic("expected statement")
+	}
+}
+
+func (p *Parser) param() *ast.Param {
+	if p.t.Type == token.Ellipses {
+		p.next()
+		return &ast.Param{
+			Name: nil,
+			Type: &ast.VarArgExpr{},
+		}
+	}
+
+	name := p.identifier()
+	p.expect(token.Colon)
+	typ := p.expr()
+
+	return &ast.Param{
+		Name: name,
+		Type: typ,
+	}
+}
+
+func (p *Parser) expr() ast.Expr {
+	return p.expr2(nil, token.PrecedenceNone)
+}
+
+func (p *Parser) expr2(left ast.Expr, prec token.Precedence) ast.Expr {
+	if left == nil {
+		left = p.unaryOperand()
+	}
+
+	for p.t.Type.Precedence() > prec {
+		left = p.binaryExpr(left)
+	}
+
+	return left
+}
+
+func (p *Parser) binaryExpr(left ast.Expr) ast.Expr {
+	t := p.t.Type
+	switch t {
+	case token.OpenParen:
+		return p.call(left)
+	case token.Dot:
+		p.next()
+		member := p.identifier()
+		return &ast.MemberExpr{Base: left, Member: member}
+	case token.Less, token.Minus, token.Plus:
+		p.next()
+		right := p.expr2(nil, t.Precedence())
+		return &ast.BinaryExpr{
+			Left:  left,
+			Op:    t,
+			Right: right,
+		}
+	default:
+		return left
+	}
+}
+
+func (p *Parser) call(base ast.Expr) ast.Expr {
+	var args []ast.Expr
+
+	p.expect(token.OpenParen)
+	for {
+		if p.accept(token.CloseParen) != nil {
+			break
+		}
+
+		args = append(args, p.expr())
+
+		if p.accept(token.CloseParen) != nil {
+			break
+		} else if p.accept(token.Comma) != nil {
+			continue
+		} else {
+			p.expect(token.Comma)
+		}
+	}
+
+	return &ast.CallExpr{
+		Base: base,
+		Args: args,
+	}
+}
+
+func (p *Parser) unaryOperand() ast.Expr {
+	switch p.t.Type {
+	case token.Identifier:
+		return p.identifier()
+	case token.String:
+		return p.string()
+	case token.Integer:
+		return p.integer()
+	case token.Return:
+		var expr ast.Expr
+		p.next()
+		if p.t.Type != token.Semicolon {
+			expr = p.expr()
+		}
+		return &ast.ReturnExpr{Value: expr}
+	case token.Func:
+		p.next()
+		return p.funcBody()
+	case token.Ellipses:
+		p.next()
+		return &ast.VarArgExpr{}
+	case token.OpenBracket:
+		return p.sliceOrManyPointer()
+	case token.If:
+		return p.ifExpr()
+	default:
+		panic(fmt.Errorf("expected unary operand but found %q", p.t.Type))
+	}
+}
+
+func (p *Parser) ifExpr() *ast.IfExpr {
+	p.expect(token.If)
+	cond := p.expr()
+	body := p.blockExpr()
+
+	return &ast.IfExpr{Cond: cond, Block: body}
+}
+
+func (p *Parser) sliceOrManyPointer() ast.Expr {
+	var manyPointer bool
+	var base ast.Expr
+
+	p.expect(token.OpenBracket)
+	switch p.t.Type {
+	case token.Asterisk:
+		p.next()
+		p.expect(token.CloseBracket)
+		manyPointer = true
+	case token.CloseBracket:
+		p.next()
+	}
+
+	base = p.expr()
+
+	if manyPointer {
+		return &ast.ManyPointerExpr{Base: base}
+	} else {
+		return &ast.SliceExpr{Base: base}
+	}
+}
+
+func (p *Parser) integer() *ast.Literal {
+	tok := p.expect(token.Integer)
+	return &ast.Literal{
+		Tok:   token.Integer,
+		Value: tok.Text,
+	}
+}
+
+func (p *Parser) string() *ast.Literal {
+	tok := p.expect(token.String)
+	return &ast.Literal{
+		Tok:   token.String,
+		Value: tok.Text,
+	}
+}
+
+func (p *Parser) identifier() *ast.Identifier {
+	tok := p.expect(token.Identifier)
+	return &ast.Identifier{
+		NamePos: tok.Pos,
+		NameEnd: tok.End,
+		Name:    tok.Text,
+	}
+}
+
+func (p *Parser) expect(tok token.Type) *token.Token {
+	if p.t == nil {
+		panic(io.ErrUnexpectedEOF)
+	}
+
+	if t := p.accept(tok); t != nil {
+		return t
+	}
+	panic(fmt.Errorf("Expected %q but found %q", tok, p.t.Type))
+}
+
+func (p *Parser) accept(tok token.Type) *token.Token {
+	if p.t == nil {
+		return nil
+	}
+
+	if p.t.Type == tok {
+		t := p.t
+		p.next()
+		return t
+	}
+	return nil
+}
+
+func (p *Parser) next() {
+	t, err := p.scn.Scan()
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			p.t = nil
+			return
+		}
+		panic(err)
+	}
+	p.t = t
+}
+
+func New(scn Scanner) *Parser {
+	return &Parser{scn: scn}
+}
+
+func NewFromReader(rd io.Reader) *Parser {
+	return New(scanner.New(rd))
+}
+
+type Scanner interface {
+	Scan() (*token.Token, error)
+}
---
+++ internal/compile/parser/parser_test.go
@@ -0,0 +1,151 @@
+package parser
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+
+	"codeberg.org/rileyq/usagi/internal/compile/ast"
+)
+
+const src = `
+const std = @import("std");
+
+func add(a: i32, b: i32) i32 {
+	return a + b;
+}
+
+func main() void {
+	std.print(add(1, 2));
+}
+`
+
+func TestParser(t *testing.T) {
+	p := NewFromReader(bytes.NewReader([]byte(src)))
+	module := p.Parse("main")
+	var b strings.Builder
+	Node(&b, module)
+	t.Log(b.String())
+}
+
+func Node(w io.Writer, node ast.Node) {
+	switch node := node.(type) {
+	case *ast.Module:
+		module(w, node)
+	}
+}
+
+func module(w io.Writer, module *ast.Module) {
+	for _, b := range module.Bindings {
+		binding(w, b)
+		io.WriteString(w, "\n")
+	}
+}
+
+func binding(w io.Writer, binding *ast.Binding) {
+	var declarators []string
+	if binding.Mode.Export() {
+		declarators = append(declarators, "export")
+	}
+	if binding.Mode.Const() {
+		declarators = append(declarators, "const")
+	}
+	if binding.Mode.Func() {
+		declarators = append(declarators, "func")
+	} else {
+		declarators = append(declarators, "let")
+	}
+	declarators = append(declarators, binding.Name.Name)
+
+	io.WriteString(w, strings.Join(declarators, " "))
+
+	if !binding.Mode.Func() {
+		if binding.Type != nil {
+			io.WriteString(w, ": ")
+			expr(w, binding.Type)
+		}
+
+		if binding.Value != nil {
+			io.WriteString(w, " = ")
+			expr(w, binding.Value)
+		}
+
+		io.WriteString(w, ";\n")
+	} else {
+		funcBody(w, binding.Value.(*ast.FuncExpr))
+	}
+}
+
+func expr(w io.Writer, x ast.Expr) {
+	switch x := x.(type) {
+	case *ast.Literal:
+		io.WriteString(w, x.Value)
+	case *ast.Identifier:
+		io.WriteString(w, x.Name)
+	case *ast.CallExpr:
+		call(w, x)
+	case *ast.BinaryExpr:
+		expr(w, x.Left)
+		io.WriteString(w, " ")
+		io.WriteString(w, x.Op.String())
+		io.WriteString(w, " ")
+		expr(w, x.Right)
+	case *ast.ReturnExpr:
+		io.WriteString(w, "return ")
+		expr(w, x.Value)
+	case *ast.MemberExpr:
+		expr(w, x.Base)
+		io.WriteString(w, ".")
+		io.WriteString(w, x.Member.Name)
+	}
+}
+
+func call(w io.Writer, cexpr *ast.CallExpr) {
+	expr(w, cexpr.Base)
+	io.WriteString(w, "(")
+	for i, arg := range cexpr.Args {
+		expr(w, arg)
+		if i < len(cexpr.Args)-1 {
+			io.WriteString(w, ", ")
+		}
+	}
+	io.WriteString(w, ")")
+}
+
+func funcBody(w io.Writer, fexpr *ast.FuncExpr) {
+	io.WriteString(w, "(")
+	for i, param := range fexpr.Params {
+		funcParam(w, param)
+		if i < len(fexpr.Params)-1 {
+			io.WriteString(w, ", ")
+		}
+	}
+	io.WriteString(w, ") ")
+	expr(w, fexpr.ReturnType)
+	io.WriteString(w, " ")
+	blockExpr(w, fexpr.Body)
+	io.WriteString(w, "\n")
+}
+
+func funcParam(w io.Writer, param *ast.Param) {
+	io.WriteString(w, param.Name.Name)
+	io.WriteString(w, ": ")
+	expr(w, param.Type)
+}
+
+func blockExpr(w io.Writer, expr *ast.BlockExpr) {
+	io.WriteString(w, "{\n")
+	for _, st := range expr.List {
+		stmt(w, st)
+	}
+	io.WriteString(w, "}")
+}
+
+func stmt(w io.Writer, stmt ast.Stmt) {
+	switch stmt := stmt.(type) {
+	case *ast.ExprStmt:
+		expr(w, stmt.X)
+		io.WriteString(w, ";\n")
+	}
+}
