2c77c4f5a06d4d836499c1075d97e53d4119ae91
Riley Quinn <merileyquinn@gmail.com>
2026-01-15 11:15:31 -0600

    add less and minus tokens

--- internal/compile/token/precedence.go
+++ internal/compile/token/precedence.go
@@ -4,13 +4,16 @@ type Precedence int
 
 const (
 	PrecedenceNone Precedence = iota
+	PrecedenceRelational
 	PrecedenceAddition
 	PrecedenceCall
 )
 
 func (t Type) Precedence() Precedence {
 	switch t {
-	case Plus:
+	case Less:
+		return PrecedenceRelational
+	case Minus, Plus:
 		return PrecedenceAddition
 	case OpenParen, Dot:
 		return PrecedenceCall
